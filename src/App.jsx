f097dbb24a5a2019aa3e409839f75a5e070d9064
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2024-05-23 21:31:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navbar from "./components/Navbar"
+import Navbar from './components/Navbar'
 import FirstPage from './components/First-Page'
 import SecondPage from './components/Second-Page'
 import Headings3 from  './components/Headings-3'
